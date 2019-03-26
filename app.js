0e18bb6c84a79a4b0bcc0403428254fab3f7f283
Mateusz Stańczak <mateusz_stanczak@pgf.com.pl>
2019-03-26 15:02:58 +0100

    aio

---
+++ app.js
@@ -0,0 +1,58 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(8888);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/event', function (req, res) {
+
+    firstMethod()
+    .then(secondMethod)
+    .then(thirdMethod);
+  
+    res.send(200);
+});
+ 
+var firstMethod = function() {
+   var promise = new Promise(function(resolve, reject){
+      setTimeout(function() {
+        console.log('first method completed'); 
+        io.emit('process1', { 'status' : 1});
+        console.log({data: 'DATAfrom1'});
+        resolve({data: 'DATAfrom1'});
+      }, 1000);
+   });
+   return promise;
+};
+ 
+var secondMethod = function(someStuff) {
+   var promise = new Promise(function(resolve, reject){
+      setTimeout(function() {
+         console.log('second method completed');
+         io.emit('process2',  { 'status' : 1});
+         console.log({newData: someStuff.data + ' DATAfrom2'});
+         resolve({newData: someStuff.data + ' DATAfrom2'});
+      }, 2000);
+   });
+   return promise;
+};
+ 
+var thirdMethod = function(someStuff) {
+   var promise = new Promise(function(resolve, reject){
+      setTimeout(function() {
+         console.log('third method completed');
+         io.emit('process3',  { 'status' : 1});
+         console.log({result: someStuff.newData + ' DATAfrom3'});
+         resolve({result: someStuff.newData + ' DATAfrom3'});
+      }, 3000);
+   });
+   return promise;
+};
+ 
+io.on('connection', function(socket){
+    socket.on('disconnect', function(){ });
+});
